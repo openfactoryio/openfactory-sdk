b3fa49b68e2cbce21845df75a72b1d406b95b5bd
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2026-06-20 16:10:12 +0000

    Add Prometheus setup and teardown to OpenFactory stack scripts

--- .devcontainer/features/infra/assets/spinup.sh
+++ .devcontainer/features/infra/assets/spinup.sh
@@ -22,15 +22,20 @@ echo "🚀  Starting OpenFactory stack..."
 SDK_PATH="/usr/local/share/openfactory-sdk"
 KAFKA_COMPOSE_FILE="${SDK_PATH}/openfactory-infra/docker-compose.yml"
 TRAEFIK_COMPOSE_FILE="${SDK_PATH}/openfactory-infra/docker-compose.traefik.yml"
-FAN_OUT_LAYER_COMPOSE_FILE="${SDK_PATH}/openfactory-fanoutlayer/docker-compose.yml"
+FAN_OUT_LAYER_COMPOSE_FILE="${SDK_PATH}/openfactory-infra/docker-compose.nats.yml"
+PROMETHEUS_COMPOSE_FILE="${SDK_PATH}/openfactory-infra/docker-compose.prometheus.yml"
 
 # Spin up containers
 echo "🐳  Deploying Kafka CLuster ..."
-docker compose -f "$KAFKA_COMPOSE_FILE" -p kafka-cluster up -d
+docker compose -f "${KAFKA_COMPOSE_FILE}" -p kafka-cluster up -d
 
 # Setup Traefik
 echo "🐳  Deploying Treafik ..."
-docker compose -f "$TRAEFIK_COMPOSE_FILE" -p traefik up -d
+docker compose -f "${TRAEFIK_COMPOSE_FILE}" -p traefik up -d
+
+# Setup Prometheus
+echo "🐳  Deploying Prometheus ..."
+docker compose -f "${PROMETHEUS_COMPOSE_FILE}" -p prometheus up -d
 
 # Setup required Kafka topics
 echo "⚙️  Setting up Kafka topics ..."
@@ -42,6 +47,14 @@ ofa setup-kafka --ksqldb-server "${KSQLDB_URL}"
 
 # Setup OpenFactory Fan-out Layer
 echo "🐳  Deploying OpenFactory fan-out layer ..."
-docker compose -f "$FAN_OUT_LAYER_COMPOSE_FILE" -p fan-out-layer up -d --scale asset-forwarder=1 --scale asset-router=1
+docker compose -f "$FAN_OUT_LAYER_COMPOSE_FILE" -p fan-out-layer up -d --scale asset-router=1
+
+# Setup OpenFactory Monitoring Layer
+echo "🏭 Deploying OpenFactory monitoring layer ..."
+ofa apps up ${SDK_PATH}/openfactory-infra/monitoring/
+
+# Setup OpenFactory Asset Forwarder
+echo "🏭 Deploying OpenFactory asset forwarder ..."
+ofa apps up ${SDK_PATH}/openfactory-infra/fanoutlayer
 
 echo "✅  OpenFactory stack is ready!"
